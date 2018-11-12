94f0f8134a4c8621f35b374170d1165a75a89427
aaron <schoeffa@gmail.com>
2018-11-12 16:33:06 -0500

    Added basic search functionality

---
+++ services/searchService.js
@@ -0,0 +1,19 @@
+"use strict";
+function SearchService($http) {
+    const self = this;
+    self.jsonPayload = null;
+
+    self.searchEdamam = (searchTerms) => {
+        return $http({
+            method: "GET",
+            url: `https://api.edamam.com/search?q=${searchTerms}&app_id=ed875414&app_key=f62e32cb2db97a3f10bf68c4bc21d258`
+        }).then((data) => {
+            self.jsonPayload = data;
+            console.log(data);
+            return self.jsonPayload;
+        })
+    };
+}
+
+
+angular.module("App").service("SearchService", SearchService);
