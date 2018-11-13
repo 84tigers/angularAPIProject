04851e8dc0ac84148118c26d9316a5a51a8a1eba
aaron <schoeffa@gmail.com>
2018-11-13 11:52:43 -0500

    Add filter functionality

--- services/searchService.js
+++ services/searchService.js
@@ -3,11 +3,22 @@ function SearchService($http) {
     const self = this;
     self.jsonPayload = null;
     self.favorites = [];
+    self.maxCal = "";
+    self.maxTime = "";
 
-    self.searchEdamam = (searchTerms) => {
+    self.searchEdamam = (searchTerms, peanut, vegan, vegetarian, lowCarb, lowFat, calories, cookTime) => {
+        if (calories) {
+            self.maxCal = `&calories=${calories}`;
+        }
+
+        if (cookTime) {
+            self.maxTime = `&time=${cookTime}`;
+        }
+
+        console.log( `https://api.edamam.com/search?q=${searchTerms}&app_id=ed875414&app_key=f62e32cb2db97a3f10bf68c4bc21d258${peanut}${vegan}${vegetarian}${lowCarb}${lowFat}${self.maxCal}${self.maxTime}`)
         return $http({
             method: "GET",
-            url: `https://api.edamam.com/search?q=${searchTerms}&app_id=ed875414&app_key=f62e32cb2db97a3f10bf68c4bc21d258`
+            url: `https://api.edamam.com/search?q=${searchTerms}&app_id=ed875414&app_key=f62e32cb2db97a3f10bf68c4bc21d258${peanut}${vegan}${vegetarian}${lowCarb}${lowFat}${self.maxCal}${self.maxTime}`
         }).then((data) => {
             self.jsonPayload = data;
             return self.jsonPayload;
